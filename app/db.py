ccf418a64ef8ea42c7d5ae445599337530140553
Gaurav Adsule <gaurav.adsule@agiliad.local>
2025-01-27 17:21:32 +0530

    changed the mysql host name

--- app/db.py
+++ app/db.py
@@ -4,10 +4,10 @@ from mysql.connector import Error
 def get_db_connection():
     try:
         connection = mysql.connector.connect(
-            host='localhost',  # Replace with your MySQL host
-            user='root',       # Replace with your MySQL username
-            password='rootpassword',  # Replace with your MySQL password
-            database='visitor_management'  # Replace with your MySQL database name
+            host='mysql'
+            user='root'
+            password='rootpassword'
+            database='visitor_management'
         )
         if connection.is_connected():
             print("Successfully connected to the database")
